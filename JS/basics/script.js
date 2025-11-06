bc7b9b9bb902732e61c4a9d2df157491dae4b87a
nishanktarar <nishanktarar@gmail.com>
2025-11-06 07:36:08 +0530

    js assignment1

---
+++ JS/basics/script.js
@@ -0,0 +1,64 @@
+// js assignment 27 oct
+
+
+// document.title
+// 'YouTube'
+
+// if html is skeleton css is clothes then js is brain 
+
+// whenever we click on a button it triggers some action 
+
+// even if the script tag is inside the section tag the output is same console is showing connected!
+
+// even with using defer results are same
+
+
+// 2+2 gives 4
+// alert hii gives a alert popup saying hii 
+// prompt ("your name?") takes an input of name in the pop up box
+
+
+
+// let msg = "i love her"; console.log(msg.slice(2,6)) gives love
+
+// msg.slice(2,6);
+
+
+// console.log("harsh")
+// console.log(20)
+/*so this code prompts age from user means it take input from user and then converts it into  number then while printing it in console it adds +5 in age.*/
+
+// let y = (5+10)*2;
+
+// console.log(y);
+
+//An expression produces a value, while a statement performs an action.
+
+// let age = 25;
+// let Name = "Harsh";
+// let isStudent = true;
+// let skills = ["js","HTML"];
+// let user = { city: "Bhopal" };
+// let x = null;
+// let y;
+// let z = Symbol("id");
+
+// console.log(typeof age); // number
+// console.log(typeof Name); //string
+// console.log(typeof isStudent); //boolean
+// console.log(typeof skills); //object
+// console.log(typeof user); //object
+// console.log(typeof x); //object
+// console.log(typeof y); //undefined
+// console.log(typeof z); //symbol
+
+
+// console.log(1/0); //infinty
+// console.log(0/0); //NaN
+// console.log(Number("abc")); //NaN
+// console.log(undefined + 1); //NaN
+
+
+
+//when you know that there will be some value in future but you don't know currently so you can give null to that
+//undefined is built in it is like it is not currently defined anywhere so by default it takes undefined 
